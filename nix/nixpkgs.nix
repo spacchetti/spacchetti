280b4302918e3ee420b7d65a714147e674e7e65f
Profpatsch <mail@profpatsch.de>
2018-07-29 15:45:14 +0200

    fix nixpkgs hash

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,5 @@
-let
-  commit = "b8561e3e20750043f238d88cc5b321f181f9d11f";
-  sha256 = "1kp8ks4jfaa2rzdbwfy8y4imvgrcb1hby7qh5vhapjxh562h9shr";
-
-in import (builtins.fetchTarball {
+import (builtins.fetchTarball {
   # nixpkgs unstable 2018-05-31
-  url = "https://github.com/NixOS/nixpkgs/archive/${commit}";
-  inherit sha256;
+  url = "https://github.com/NixOS/nixpkgs/archive/b8561e3e20750043f238d88cc5b321f181f9d11f.tar.gz";
+  sha256 = "15fs3pmsvca3icpbjyggnc7ihb60g7idfbpw58zsskxl2vpwil2c";
 })
