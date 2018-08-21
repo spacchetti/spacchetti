0a14cba6f0767d49172a2c650522eb801fa88d71
Profpatsch <mail@profpatsch.de>
2018-08-22 00:05:11 +0200

    scripts: add Prefetch.hs
    To run:
    
        nix-shell --pure --run "runhaskell scripts/Prefetch.hs"
    
    Prefetch.hs is an experiment of calling directly into the Dhall
    library to read spacchetti. Dhall has no concept of a map of values
    with uniform type, so we match into the syntax tree manually and then
    check each package.
    
    It will use libnix to prefetch the git repositories and output a json
    of hashes. The prefetching clones five repositories at a time and
    retries on network errors.

--- shell.nix
+++ shell.nix
@@ -2,6 +2,48 @@
 
 with pkgs;
 
+let
+  h = haskellPackages.override {
+    overrides = self: super: {
+      # libnix pinning
+      libnix = super.mkDerivation {
+        pname = "libnix";
+        version = "0.3.0";
+        src = fetchFromGitHub {
+          owner = "Profpatsch";
+          repo = "libnix-haskell";
+          rev = "b891f4f425da5fa3e7e06bce98c56cf6cb6c876c";
+          sha256 = "00wbz83mlgqq5ml7ysdw6wfr74grpac7sj3v9cylvhy2blx922yp";
+        };
+        license = lib.licenses.gpl3;
+        buildDepends = with self; [ protolude aeson errors tasty-hunit ];
+        # buildTools = [ nix ];
+        doCheck = false;
+      };
+      # package for creating a shell with all tools
+      # spacchetti-tools =
+      #   let packages = with self; [ libnix dhall ];
+      #   in super.mkDerivation {
+      #     pname = "spacchetti-tools";
+      #     version = "none";
+      #     license = "none";
+      #     buildDepends = packages;
+      #     buildTools = with self;
+      #       [ cabal-install (hoogleLocal { inherit packages; }) ];
+      #   };
+    };
+  };
+
+  hDeps = with h; [
+    libnix
+    dhall
+    aeson-pretty
+    either
+    async-pool
+    insert-ordered-containers
+    retry
+  ];
+in
 mkShell {
   buildInputs = [
     dhall
@@ -9,9 +51,20 @@ mkShell {
     perl
     jq
     psc-package
-    # TODO: remove once https://github.com/dhall-lang/dhall-haskell/issues/504 is fixed
-    glibcLocales
+  ] ++ hDeps;
+  nativeBuildInputs = [
+    h.cabal-install
+    (h.hoogleLocal { packages = hDeps; })
+    (h.ghcWithPackages (lib.const hDeps))
+    nix-prefetch-scripts
   ];
-  # TODO: like glibc locales
-  LC_ALL = "en_US.UTF-8";
+
+  # for ghc
+  # we copy a lot of stuff of the haskellPackages env builder,
+  # because it is unfortunately not very composable.
+  LANG = "en_US.UTF-8";
+  # TODO: remove once https://github.com/dhall-lang/dhall-haskell/issues/504 is fixed
+  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+  inherit (h.libnix.env) shellHook;
 }
+
