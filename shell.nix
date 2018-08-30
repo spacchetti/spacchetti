0ee95ed3d194148ab1991e75c749658ebbab2648
Profpatsch <mail@profpatsch.de>
2018-08-30 23:01:19 +0200

    shell.nix: Add smaller shell environment.
    A smaller shell, that does not contain the Haskell script
    dependencies. Call with:
    
        nix-shell -A small

--- shell.nix
+++ shell.nix
@@ -17,20 +17,8 @@ let
         };
         license = lib.licenses.gpl3;
         buildDepends = with self; [ protolude aeson errors tasty-hunit ];
-        # buildTools = [ nix ];
         doCheck = false;
       };
-      # package for creating a shell with all tools
-      # spacchetti-tools =
-      #   let packages = with self; [ libnix dhall ];
-      #   in super.mkDerivation {
-      #     pname = "spacchetti-tools";
-      #     version = "none";
-      #     license = "none";
-      #     buildDepends = packages;
-      #     buildTools = with self;
-      #       [ cabal-install (hoogleLocal { inherit packages; }) ];
-      #   };
     };
   };
 
@@ -43,28 +31,38 @@ let
     insert-ordered-containers
     retry
   ];
-in
-mkShell {
-  buildInputs = [
-    dhall
-    dhall-json
-    perl
-    jq
-    psc-package
-  ] ++ hDeps;
-  nativeBuildInputs = [
-    h.cabal-install
-    (h.hoogleLocal { packages = hDeps; })
-    (h.ghcWithPackages (lib.const hDeps))
-    nix-prefetch-scripts
-  ];
 
-  # for ghc
-  # we copy a lot of stuff of the haskellPackages env builder,
-  # because it is unfortunately not very composable.
-  LANG = "en_US.UTF-8";
-  # TODO: remove once https://github.com/dhall-lang/dhall-haskell/issues/504 is fixed
-  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
-  inherit (h.libnix.env) shellHook;
-}
+  overrideAttrs = attrs: f: attrs // f attrs;
+
+  smallShellArgs = {
+    buildInputs = [
+      dhall
+      dhall-json
+      perl
+      jq
+      psc-package
+    ];
+  };
+
+  fullShellArgs = overrideAttrs smallShellArgs (old: {
+    buildInputs = old.buildInputs ++ hDeps;
+    nativeBuildInputs = [
+      h.cabal-install
+      (h.hoogleLocal { packages = hDeps; })
+      (h.ghcWithPackages (lib.const hDeps))
+      nix-prefetch-scripts
+    ];
+    # for ghc
+    # we copy a lot of stuff of the haskellPackages env builder,
+    # because it is unfortunately not very composable.
+    LANG = "en_US.UTF-8";
+    # TODO: remove once https://github.com/dhall-lang/dhall-haskell/issues/504 is fixed
+    LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+    inherit (h.libnix.env) shellHook;
+  });
+
+in
+mkShell (fullShellArgs // {
+  passthru.small = mkShell smallShellArgs;
+})
 
