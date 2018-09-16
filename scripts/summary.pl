3994b15fa883be13f434a468ee0b1a36447b9ed4
justinwoo <moomoowoo@gmail.com>
2018-09-16 16:24:05 +0900

    make summary script

---
+++ scripts/summary.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use warnings;
+
+# summary of target packages.json file
+
+my $target = $ARGV[0];
+
+my @packages = `jq 'keys[]' $target -r`;
+
+foreach $package (@packages) {
+  chomp($package);
+  my $version = `jq '."$package".version' $target -r`;
+  my @dependencies = `jq '."$package".dependencies[]' $target -r`;
+  my $deps = join(",", @dependencies);
+  $version =~ s/\n//g;
+  $deps =~ s/\n//g;
+  print "$package;$version;$deps\n";
+}
