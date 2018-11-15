c3eddba051a8f557e8051468db528e0abe63475a
justinwoo <moomoowoo@gmail.com>
2018-11-15 05:56:44 +0200

    update easy-purescript-nix

--- easy-purescript.nix
+++ easy-purescript.nix
@@ -4,8 +4,8 @@ let
   easy-ps = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "43b18de";
-    sha256 = "0c36pxafmlyq643kvyq61rw8z3h5dbg5gjb6mi7rxb5bsasqwxp8";
+    rev = "c6f4e8c4babe51b30aa44c7f6cac426775ab9afb";
+    sha256 = "1bb99qnnisnpin6q6nw1klf7nsjaimgbamfw70c19w0ya0sl15g0";
   });
 in pkgs.stdenv.mkDerivation {
   name = "easy-ps-test";
